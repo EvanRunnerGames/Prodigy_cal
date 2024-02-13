ccf11c14e4fedaaf69d5a4733855e11d4a8b4d21
EvanRunner Games <137564315+EvanRunnerGames@users.noreply.github.com>
2024-02-12 18:14:57 -0600

    Update circle.py

--- circle.py
+++ circle.py
@@ -6,7 +6,7 @@ while True:
     _1to4 = input(Fore.BLUE + Back.WHITE + "What do you already have Radius:Diameter:Circumfrance:Area; ")
     #Radius Math
     if _1to4.lower() == "radius" or _1to4.lower() == "r":
-        radius =  input(Fore.WHITE + "What is the Radius: ")
+        radius =  input(Fore.BLUE + "What is the Radius: ")
         print("Radius: " + str(radius))
         diameter = int(radius) * 2
         print("Diameter: " + str(diameter))
@@ -14,7 +14,7 @@ while True:
         print("Area: " + str(math.pi * float(radius) * float(radius)))
     #Diameter Math
     if _1to4.lower() == "diameter" or _1to4.lower() == "d":
-        diameter =  input(Fore.WHITE + "What is the Diameter: ")
+        diameter =  input(Fore.BLUE + "What is the Diameter: ")
         print(Fore.GREEN + "Radius: " + str(int(float(diameter) / 2)))
         print(Fore.GREEN + "Diameter: " + str(diameter))
         print(Fore.GREEN + "Circumference: " + str((math.pi * float(diameter))))
@@ -24,7 +24,7 @@ while True:
     #Circumfrance Math
         
     if _1to4.lower() == "circumfrance" or _1to4.lower() == "c":
-        circumfrance =  float(input(Fore.WHITE + "What is the Circumfrance: "))
+        circumfrance =  float(input(Fore.BLUE + "What is the Circumfrance: "))
         diameter = circumfrance / math.pi
         radius = diameter / 2
         print(Fore.GREEN + "Radius: " + str(radius))
@@ -35,7 +35,7 @@ while True:
     #Area Math
         
     if _1to4.lower() == "area" or _1to4.lower() == "a":
-        area =  float(input(Fore.WHITE + "What is the Area: "))
+        area =  float(input(Fore.BLUE + "What is the Area: "))
         radius = math.sqrt(area / math.pi)
         diameter = radius + radius
         print(Fore.GREEN + "Radius: " + str(radius))
